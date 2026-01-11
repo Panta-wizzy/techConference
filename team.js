e8bcba97b1cf24a8da806ced13ad9157e1278c4f
Panta-wizzy <pantawizzy992@gmail.com>
2026-01-12 02:28:12 +0300

    Add files via upload

---
+++ team.js
@@ -0,0 +1,15 @@
+const cards = document.querySelectorAll('.team-card');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+}, {
+  threshold: 0.1
+});
+
+cards.forEach(card => {
+  observer.observe(card);
+});
